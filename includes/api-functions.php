f6eb20ba9b247dbec1070f24d34f057e00554d3e
WebDJ <webdj.agency@gmail.com>
2020-08-25 17:01:25 +0300

    Get top 200 instead of 100 games #26

--- includes/api-functions.php
+++ includes/api-functions.php
@@ -19,9 +19,26 @@ if ( ! defined( 'ABSPATH' ) ) {
  */
 function tp_twitch_get_top_games_from_api( $args = array() ) {
 
-	$results = tp_twitch()->api->get_top_games( $args );
+    // Get first 100 games
+	$response = tp_twitch()->api->get_top_games( $args );
 
-    return ( isset( $results['data'] ) && is_array( $results['data'] ) && sizeof( $results['data'] ) > 0 ) ? $results['data'] : null;
+    if ( ! is_array( $response['data'] ) || empty( $response['data'] ) )
+        return null;
+
+    $result = $response['data'];
+
+    // Get second 100 games
+    if ( ! empty( $response['pagination']['cursor'] ) ) {
+
+        $args['after'] = $response['pagination']['cursor'];
+
+        $paginated = tp_twitch()->api->get_top_games( $args );
+
+        if ( is_array( $paginated['data'] ) && ! empty( $paginated['data'] ) )
+            $result = array_merge( $result, $paginated['data'] );
+    }
+
+    return $result;
 }
 
 /**
