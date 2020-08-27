ac6640eeacec3785c1714695d09b854c6f6c7c46
flowdee <mail@flowdee.de>
2020-08-27 08:50:34 +0200

    updated adding of missing games

--- includes/hooks.php
+++ includes/hooks.php
@@ -52,44 +52,47 @@ add_filter( 'tp_twitch_display_streams', 'tp_twitch_manipulate_display_streams',
  * @param $games
  * @return mixed
  */
-function tp_twitch_extend_available_games( $games ) {
+function tp_twitch_add_missing_games( $games ) {
 
-    $games[490379] = array(
-        'id'          => 490379,
-        'name'        => "Tom Clancy's Ghost Recon: Wildlands",
-        'box_art_url' => 'https://static-cdn.jtvnw.net/ttv-boxart/Tom%20Clancy%27s%20Ghost%20Recon:%20Wildlands-{width}x{height}.jpg'
+    $games_missing = array(
+        array(
+            'id'          => 490379,
+            'name'        => "Tom Clancy's Ghost Recon: Wildlands",
+            'box_art_url' => 'https://static-cdn.jtvnw.net/ttv-boxart/Tom%20Clancy%27s%20Ghost%20Recon:%20Wildlands-{width}x{height}.jpg'
+        ),
+        array(
+            'id'          => 510146,
+            'name'        => "MLB The Show 19",
+            'box_art_url' => 'https://static-cdn.jtvnw.net/ttv-boxart/MLB%20The%20Show%2019-{width}x{height}.jpg'
+        ),
+        array(
+            'id'          => 511496,
+            'name'        => "Out Of The Park Baseball 20",
+            'box_art_url' => 'https://static-cdn.jtvnw.net/ttv-boxart/Out%20of%20the%20Park%20Baseball%2019-{width}x{height}.jpg'
+        ),
+        array(
+            'id'          => 500626,
+            'name'        => "Soulcalibur VI",
+            'box_art_url' => 'https://static-cdn.jtvnw.net/ttv-boxart/Soulcalibur%VI-{width}x{height}.jpg'
+        ),
+        array(
+            'id'          => 513170,
+            'name'        => "NHL 20",
+            'box_art_url' => 'https://static-cdn.jtvnw.net/ttv-boxart/NHL%2020-{width}x{height}.jpg'
+        ),
+        array(
+            'id'          => 518711,
+            'name'        => "EA Sports UFC 4",
+            'box_art_url' => 'https://static-cdn.jtvnw.net/ttv-boxart/EA%20Sports%20UFC%204-{width}x{height}.jpg'
+        )
     );
 
-    $games[510146] = array(
-        'id'          => 510146,
-        'name'        => "MLB The Show 19",
-        'box_art_url' => 'https://static-cdn.jtvnw.net/ttv-boxart/MLB%20The%20Show%2019-{width}x{height}.jpg'
-    );
+    foreach ( $games_missing as $game ) {
 
-    $games[511496] = array(
-        'id'          => 511496,
-        'name'        => "Out Of The Park Baseball 20",
-        'box_art_url' => 'https://static-cdn.jtvnw.net/ttv-boxart/Out%20of%20the%20Park%20Baseball%2019-{width}x{height}.jpg'
-    );
-
-    $games[500626] = array(
-        'id'          => 500626,
-        'name'        => "Soulcalibur VI",
-        'box_art_url' => 'https://static-cdn.jtvnw.net/ttv-boxart/Soulcalibur%VI-{width}x{height}.jpg'
-    );
-
-    $games[513170] = array(
-        'id'          => 513170,
-        'name'        => "NHL 20",
-        'box_art_url' => 'https://static-cdn.jtvnw.net/ttv-boxart/NHL%2020-{width}x{height}.jpg'
-    );
-
-    $games[518711] = array(
-        'id'          => 518711,
-        'name'        => "EA Sports UFC 4",
-        'box_art_url' => 'https://static-cdn.jtvnw.net/ttv-boxart/EA%20Sports%20UFC%204-{width}x{height}.jpg'
-    );
+        if ( ! isset ( $games[ $game['id'] ] ) )
+            $games[ $game['id'] ] = $game;
+    }
 
     return $games;
 }
-add_filter( 'tp_twitch_games', 'tp_twitch_extend_available_games', 10 );
+add_filter( 'tp_twitch_games', 'tp_twitch_add_missing_games', 99 );
