193b1cb1299b7259b9e636eb51fa789a18d81036
flowdee <flowdee@users.noreply.github.com>
2020-08-27 08:35:27 +0200

    Update hooks.php

--- includes/hooks.php
+++ includes/hooks.php
@@ -49,14 +49,6 @@ add_filter( 'tp_twitch_display_streams', 'tp_twitch_manipulate_display_streams',
 /**
  * Extend available games
  *
- * Info: By default we only have access to the top games, so we may have to extend this list
- *
- * Several ways for getting the 'EA Sports UFC 4' game ID:
- *  1. Chrome browser DevTools -> Network -> XHR: one of the 'gql' requests has the response like:
- *   [{"data":{"game":{"id":"518711","displayName":"EA Sports UFC 4","__typename":"Game"}} ...
- *
- * 2. curl -H 'Client-ID: <client id>' -H 'Authorization: Bearer <token>' -X GET 'https://api.twitch.tv/helix/games?name=EA%20Sports%20UFC%204'
- *
  * @param $games
  * @return mixed
  */
